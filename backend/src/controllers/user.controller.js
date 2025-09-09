3a6012d00b6be0861e820998a818e8537651ff8c
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-09-09 15:18:09 +0100

    get my friends and get recommended users done

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,5 @@
+import User from "../models/user.model.js";
+
 export async function getRecommendedUsers(req, res) {
     try {
         const currentUserId = req.user._id;
@@ -10,11 +12,20 @@ export async function getRecommendedUsers(req, res) {
                 { isOnboarded: true }
             ]
         });
-        res.status(200).json({ success: true, users: recommendedUsers });
+        res.status(200).json(recommendedUsers);
     } catch (error) {
         console.error("Error fetching recommended users:", error.message);
         res.status(500).json({ success: false, message: "Error fetching recommended users" });
     }
 }
     
-export async function getMyFriends(req, res) {}
+export async function getMyFriends(req, res) {
+    try {
+        const user = await User.findById(req.user._id).select("friends")
+        .populate("friends", "fullName profilePic nativeLanguage learningLanguage");
+        res.status(200).json(user.friends || []);
+    } catch (error) {
+        console.error("Error fetching friends:", error.message);
+        res.status(500).json({ success: false, message: "Error fetching friends" });
+    }
+}
