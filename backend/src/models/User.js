611c9641bd417531b5dd90f3750bdbf10687d0b6
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-09-09 10:00:47 +0100

    password hash

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -52,7 +52,6 @@ const userSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
 
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
@@ -65,5 +64,8 @@ userSchema.pre('save', async function (next) {
         next(error);
     }
 });
+
+const User = mongoose.model("User", userSchema);
+
 
 export default User;
