6d99f27a8aa292db5c381a255e9f14fde01f1fcb
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-09-11 11:47:25 +0100

    seetings and pages created on the frontend

---
+++ frontend/src/pages/CallPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CallPage = () => {
+  return (
+    <div>
+        <h1 className='text-5xl'>Call Page</h1>
+    </div>
+  )
+}
+
+export default CallPage
---
+++ frontend/src/pages/NotificationPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotificationPage = () => {
+  return (
+    <div>
+        <h1 className='text-5xl'>Notification Page</h1>
+    </div>
+  )
+}
+
+export default NotificationPage
