68fd4a31c787568d17a4bd138ae0628f15b243d0
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-09-27 22:28:46 +0100

    update db

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -5,7 +5,13 @@ import mongoose from "mongoose";
 export const connectDB =async () =>{
 
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+  bufferMaxEntries: 0, // Disable mongoose buffering
+  bufferCommands: false, // Disable mongoose buffering
+});
         console.log(`MongoDB connected: ${conn.connection.host}`);
 
     } catch (error) {
