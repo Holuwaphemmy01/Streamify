fccd6c8503995173e681090850a39ab59cbf7ac7
Holuwaphemmy <jacoboluwafemi2002@gmail.com>
2025-09-29 12:18:05 +0100

    update on cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@
  const PORT = process.env.PORT; 
 
    app.use(cors({
-     origin: "https://streamify-jfol.vercel.app/",
+     origin: ["https://streamify-jfol.vercel.app", "http://localhost:5173"],
      credentials: true, // allow frontend to send cookies
    }));
 
